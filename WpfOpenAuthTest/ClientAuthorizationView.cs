484786b13b545394849a04da7c238111105f93e4
Gerben de Vries <gerben.de-vries1_ext@michelin.com>
2018-11-28 16:44:03 +0100

    Removed the authorize2 popup, moved its content directly in the Mainwindow

---
+++ WpfOpenAuthTest/ClientAuthorizationView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace WpfOpenAuthTest
+{
+    class ClientAuthorizationView : DotNetOpenAuth.OAuth2.ClientAuthorizationView
+    {
+
+        public WindowsFormsHost Host{ get; set; }
+
+        public ClientAuthorizationView()
+        {
+            base.HandleCreated += ClientAuthorizationView_HandleCreated;
+        }
+
+        private void ClientAuthorizationView_HandleCreated(object sender, EventArgs e)
+        {
+            browser = (WebBrowser) Controls[0];
+            browser.Navigated += Browser_Navigated;  
+        }
+
+        private void Browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            bool shouldBeHidden = e.Url.Host.Equals(System.Net.IPAddress.Loopback.ToString());
+            Host.Visibility = shouldBeHidden ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private WebBrowser browser { get; set; }
+
+        public void Reload()
+        {
+            Uri authorizationUrl = this.Client.RequestUserAuthorization(this.Authorization);
+            browser.Navigate(authorizationUrl.AbsoluteUri);
+        }
+
+    }
+}
--- WpfOpenAuthTest/MainWindow.xaml.cs
+++ WpfOpenAuthTest/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net;
 using DotNetOpenAuth.Messaging;
 using System.IO;
+using System.Threading;
 
 namespace WpfOpenAuthTest
 {
@@ -24,44 +25,74 @@ namespace WpfOpenAuthTest
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private async void MakeUserInfoRequest()
-        {
             var authServer = new AuthorizationServerDescription
             {
                 AuthorizationEndpoint = new Uri(_EndPoints.Authorization),
                 TokenEndpoint = new Uri(_EndPoints.Token)
             };
 
+            var client = new UserAgentClient(authServer, ClientId, ClientSecret);
+            clientAuthorizationView.Host = clientAuthorizationViewHost;
+            clientAuthorizationView.Client = client;
+
+            clientAuthorizationView.Authorization.Scope.AddRange(OAuthUtilities.SplitScopes(Scopes));
+            clientAuthorizationView.Authorization.Callback = new Uri(string.Format("http://{0}/", IPAddress.Loopback));
+        }
+
+        private void clientAuthorizationView_Completed(object sender, ClientAuthorizationCompleteEventArgs e)
+        {
+            AuthorizationSuccess = e.Authorization != null && e.Authorization.AccessToken != null;
+            //clientAuthorizationViewHost.Visibility = Visibility.Hidden;
+            AuthorizationGrantedSemaphore.Release();
+        }
+
+        private readonly SemaphoreSlim AuthorizationGrantedSemaphore = new SemaphoreSlim(0);
+        private bool AuthorizationSuccess { get; set; }
+
+        private async void MakeUserInfoRequest()
+        {
+            //var authServer = new AuthorizationServerDescription
+            //{
+            //    AuthorizationEndpoint = new Uri(_EndPoints.Authorization),
+            //    TokenEndpoint = new Uri(_EndPoints.Token)
+            //};
+            clientAuthorizationViewHost.Visibility = Visibility.Visible;
+
             try
             {
-                var client = new UserAgentClient(authServer, ClientId, ClientSecret);
+                //var client = new UserAgentClient(authServer, ClientId, ClientSecret);
 
-                var authorizePopup = new Authorize2(client);
-                authorizePopup.Authorization.Scope.AddRange(OAuthUtilities.SplitScopes(Scopes));
-                authorizePopup.Authorization.Callback = new Uri(string.Format("http://{0}/", IPAddress.Loopback));
-                authorizePopup.Owner = this;
+                //// var authorizePopup = new Authorize2(client);
+                //clientAuthorizationView.Client = client;
+
+                //clientAuthorizationView.Authorization.Scope.AddRange(OAuthUtilities.SplitScopes(Scopes));
+                //clientAuthorizationView.Authorization.Callback = new Uri(string.Format("http://{0}/", IPAddress.Loopback));
+                // clientAuthorizationView.Owner = this;
                 // authorizePopup.ClientAuthorizationView.RequestImplicitGrant = ImplicitGrant; (only available in DotNetOpenAuth 5.0-beta)
+                clientAuthorizationView.Reload();
 
-                bool? result = authorizePopup.ShowDialog();
-                if (result.HasValue && result.Value)
+                //clientAuthorizationViewHost.Visibility = Visibility.Visible;
+                await AuthorizationGrantedSemaphore.WaitAsync();
+                
+                if (AuthorizationSuccess)
                 {
+                    AuthorizationSuccess = false;
+
                     var requestUri = new Uri((_EndPoints as IAM.OpenIdConnect.OIDCEndPoints).UserInfo);
 
                     var request = (HttpWebRequest)WebRequest.Create(requestUri);
                     request.Method = "GET";
-                    client.AuthorizeRequest(request, authorizePopup.Authorization);
+                    clientAuthorizationView.Client.AuthorizeRequest(request, clientAuthorizationView.Authorization);
 
                     using (var reader = new StreamReader(request.GetResponse().GetResponseStream()))
                     {
                         string responseString = reader.ReadToEnd();
+                        MessageBox.Show(responseString);
                         // var response = (JObject)JsonConvert.DeserializeObject(responseString);
                     }
-
                 }
             }
-            //TODO instead of using a popup, move this code to the current window
 
             catch (ProtocolException ex)
             {
